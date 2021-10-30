42e3a0ca59daf7a7022292829bcbaf2f9d06f3d1
Your Name <you@example.com>
2021-10-30 08:12:07 +0800

    Spliting....

--- MdeModulePkg/Application/A/Devices/Store/L2_DEVICE_Store.h
+++ MdeModulePkg/Application/A/Devices/Store/L2_DEVICE_Store.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <Global/Global.h>
+
 
 EFI_STATUS L1_STORE_READ(UINT8 deviceID, UINT64 StartSectorNumber, UINT16 ReadSize, UINT8 *pBuffer);
 
 
 EFI_STATUS L1_STORE_Write(UINT8 deviceID, UINT64 StartSectorNumber, UINT16 WriteSize, UINT8 *pBuffer);
 
+EFI_STATUS L2_STORE_PartitionAnalysis();
+
+void L2_STORE_TextDevicePathAnalysis(CHAR16 *p, DEVICE_PARAMETER *device, UINTN count1);
 
