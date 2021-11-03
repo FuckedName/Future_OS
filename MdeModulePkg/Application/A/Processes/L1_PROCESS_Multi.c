bb5ff1f797accca2ad04b870e675562062045e5b
Your Name <you@example.com>
2021-11-03 16:20:18 +0800

    add header note

--- MdeModulePkg/Application/A/Processes/L1_PROCESS_Multi.c
+++ MdeModulePkg/Application/A/Processes/L1_PROCESS_Multi.c
@@ -1,3 +1,27 @@
+
+/*************************************************
+    .
+    File name:      	A.c
+    Author：	        任启红
+    ID：		00001
+    Version: Date:  	202107
+    Description:    	整个模块的主入口函数
+    Others:         	无
+    History:        	无
+	    1.  Date:
+		    Author: 
+		    ID:
+		    Modification:
+		    
+	    2.  Date:
+		    Author: 
+		    ID:
+		    Modification:
+*************************************************/
+
+
+
+
 #include <L1_PROCESS_Multi.h>
 //#include <Device/Keyboard/L2_DEVICE_Keyboard.h>
 //#include <Device/Mouse/L2_DEVICE_Mouse.h>
