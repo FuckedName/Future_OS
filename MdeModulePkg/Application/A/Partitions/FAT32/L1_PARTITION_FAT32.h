9310db0645d0e181cfcda1f8bc6086c093dc8a5f
Your Name <you@example.com>
2021-11-03 16:10:06 +0800

    Revert "add note header."
    This reverts commit 2b38f8ea8f17b15bdf68e206b3107cd7bd81364b.

--- MdeModulePkg/Application/A/Partitions/FAT32/L1_PARTITION_FAT32.h
+++ MdeModulePkg/Application/A/Partitions/FAT32/L1_PARTITION_FAT32.h
@@ -1,25 +1,3 @@
 #pragma once
-/*************************************************
-    .
-    File name:      	A.c
-    Author：	        任启红
-    ID：		00001
-    Version: Date:  	202107
-    Description:    	整个模块的主入口函数
-    Others:         	无
-    History:        	无
-	    1.  Date:
-		    Author: 
-		    ID:
-		    Modification:
-		    
-	    2.  Date:
-		    Author: 
-		    ID:
-		    Modification:
-*************************************************/
-
-
-
 
 
