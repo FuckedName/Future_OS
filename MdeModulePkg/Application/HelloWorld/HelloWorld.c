171d0f7df6daf35d052d1dd75da47c9217a5ed82
Ren qihong <you@example.com>
2022-01-27 09:20:13 +0800

    Application can surpport ASSERT. happy more.

--- MdeModulePkg/Application/HelloWorld/HelloWorld.c
+++ MdeModulePkg/Application/HelloWorld/HelloWorld.c
@@ -1,11 +1,12 @@
 #include <Uefi.h>
 #include <Library/PcdLib.h>
 #include <Library/UefiLib.h>
+#include <Library/DebugLib.h>
 #include <Library/UefiApplicationEntryPoint.h>
 
 EFI_STATUS EFIAPI UefiMain (IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable)
 {
     Print(L"Hello Renqihong jiayou!!!!\r\n");
-
+    ASSERT_EFI_ERROR(-1);
     return EFI_SUCCESS;
 }
