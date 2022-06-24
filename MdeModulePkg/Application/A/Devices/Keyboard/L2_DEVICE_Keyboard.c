ae087d42ffdb21266b3f2bcb554e555ff16b684f
Ren qihong <you@example.com>
2022-06-24 17:57:39 +0800

    Adapt to realize curl command.

--- MdeModulePkg/Application/A/Devices/Keyboard/L2_DEVICE_Keyboard.c
+++ MdeModulePkg/Application/A/Devices/Keyboard/L2_DEVICE_Keyboard.c
@@ -151,6 +151,10 @@ VOID L2_KEYBOARD_CommandHandle()
     {
     	L2_APPLICATIONS_Command_ls(parameters);
     }
+    else if (L1_STRING_Compare2(parameters[0], "curl") == 0)  //When click 'shutdown' then Shutdown System
+    {
+    	L2_APPLICATIONS_Command_curl(parameters);
+    }
 
 	//显示输入的按键
 	L2_DEBUG_Print1(DISPLAY_KEYBOARD_X, DISPLAY_KEYBOARD_Y, "%a keyboard_input_count: %04d ", pKeyboardInputBuffer, keyboard_input_count);	  
